71044115923426534a9fa857b2a1aded90c1c9d7
cdolm92 <cdolm92@gmail.com>
2016-03-28 23:08:54 -0400

    added custom UICollectionViewCell

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Christella on 3/28/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
