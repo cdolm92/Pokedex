6489db738ad5219d8a63b10c563a04175d0e410e
cdolm92 <cdolm92@gmail.com>
2016-03-28 22:05:17 -0400

    added Pokemon class

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Christella on 3/28/16.
+//  Copyright © 2016 Christella. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedeId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
