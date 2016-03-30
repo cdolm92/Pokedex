70282f483e21857a372b4ccf14f615b792d61471
cdolm92 <cdolm92@gmail.com>
2016-03-30 01:02:20 -0400

    parsed pokeapi.co json and made a 2nd request to get pokemon description

--- pokedex/Pokemon.swift
+++ pokedex/Pokemon.swift
@@ -60,8 +60,6 @@ class Pokemon {
                         self._defense = "\(defense)"
                     }
                     
-                    
-                    
                     print(self._weight)
                     print(self._height)
                     print(self._attack)
@@ -86,7 +84,34 @@ class Pokemon {
                     }
                     
                     print(self._type)
+                    
+                    if let descArr = dict["descriptions"] as? [Dictionary<String, String>] where descArr.count > 0 {
+                        
+                        
+                        if let url = descArr[0]["resource_uri"] {
+                            let nsurl = NSURL(string: "\(URL_BASE)\(url)")!
+                            
+                            Alamofire.request(.GET, nsurl).responseJSON { response in
+                                let desResult = response.result
+                                
+                                if let descDict = desResult.value as? Dictionary<String,AnyObject> {
+                                    
+                                    if let description = descDict["description"] as? String {
+                                        self._description = description
+                                        print(self._description)
+                                    }
+                                }
+                                
+                                completed()
+                    }
+                        
+                    } else {
+                        self._description = ""
+                        
+                    }
                 }
             }
         }
+    }
 }
+
